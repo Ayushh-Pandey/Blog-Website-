6715567692a1653776b8447f8123da602b861e4e
Ayush pandey <ayushpandey939@gmail.com>
2023-10-03 00:20:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-// const Router = require("./routes/route");
+const Router = require("./routes/route");
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
